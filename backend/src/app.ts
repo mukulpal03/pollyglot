d212395a3ab21ad4cb7fbe62c35a8d39c5b86bbe
Mukul Pal <mukulpal0333@gmail.com>
2026-04-24 16:53:42 +0530

    feat: add helmet lib for security headers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import helmet from "helmet";
 import translateRouter from "./routes/translate";
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") app.use(helmet());
 
 app.use(express.json());
 
