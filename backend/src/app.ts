a45cc7a98a4e588bbd7886582d764fc7421ccc4f
Mukul Pal <mukulpal0333@gmail.com>
2026-04-24 17:16:44 +0530

    feat: setup cors in the backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
 import translateRouter from "./routes/translate";
 
 const app = express();
@@ -7,6 +8,14 @@ const app = express();
 if (process.env.NODE_ENV === "production") app.use(helmet());
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN_URL,
+    credentials: true,
+  }),
+);
 
 app.use("/api", translateRouter);
 
